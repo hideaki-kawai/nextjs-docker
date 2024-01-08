3ef171e46b7bfbe50a33b55890244fa280566d80
hkawai <h.kawai.tech@gmail.com>
2024-01-09 00:04:55 +0900

    fix: Streaming HTMLの実装

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,15 @@
 import { NotesList } from '@/components/notesList';
+import { Spinner } from '@/components/spinner';
 import { TimerCounter } from '@/components/timerCounter';
+import { Suspense } from 'react';
 
 export default function Home() {
   return (
     <main>
       <div className="m-10 text-center">
-        Next.js Docker!!
-        <NotesList />
+        <Suspense fallback={<Spinner color="border-green-500" />}>
+          <NotesList />
+        </Suspense>
         <TimerCounter />
       </div>
     </main>
