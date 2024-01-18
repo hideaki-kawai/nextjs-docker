8c63cb579982983d0c2d2eca56f99e97207bc145
hkawai <h.kawai.tech@gmail.com>
2024-01-18 22:57:40 +0900

    feat: ナビゲーションバーにログインボタンを実装

--- src/components/navBar.tsx
+++ src/components/navBar.tsx
@@ -3,32 +3,37 @@ import Link from 'next/link';
 export const NavBar = async () => {
   return (
     <header className="bg-gray-800 p-4">
-      <nav className="space-x-4">
-        <Link
-          href="/"
-          className="rounded bg-gray-700 px-3 py-2 text-white hover:bg-gray-500"
-        >
-          Home
-        </Link>
-        <Link
-          href="/blogs"
-          className="rounded bg-gray-700 px-3 py-2 text-white hover:bg-gray-500"
-        >
-          Nested Layout with Blogs
-        </Link>
-        <Link
-          href="/streaming-sr"
-          className="rounded bg-gray-700 px-3 py-2 text-white hover:bg-gray-500"
-        >
-          Streaming SR
-        </Link>
-        <Link
-          href="/auth"
-          className="rounded bg-gray-700 px-3 py-2 text-white hover:bg-gray-500"
-        >
-          Authorization
-        </Link>
-      </nav>
+      <div className="flex justify-between items-center">
+        <nav className="space-x-4">
+          <Link
+            href="/"
+            className="rounded bg-gray-700 px-3 py-2 text-white hover:bg-gray-500"
+          >
+            Home
+          </Link>
+          <Link
+            href="/blogs"
+            className="rounded bg-gray-700 px-3 py-2 text-white hover:bg-gray-500"
+          >
+            Nested Layout with Blogs
+          </Link>
+          <Link
+            href="/streaming-sr"
+            className="rounded bg-gray-700 px-3 py-2 text-white hover:bg-gray-500"
+          >
+            Streaming SR
+          </Link>
+          <Link
+            href="/auth"
+            className="rounded bg-gray-700 px-3 py-2 text-white hover:bg-gray-500"
+          >
+            Authorization
+          </Link>
+        </nav>
+        <button className="rounded bg-blue-500 px-3 py-2 text-white hover:bg-blue-400">
+          Login
+        </button>
+      </div>
     </header>
   );
 };
